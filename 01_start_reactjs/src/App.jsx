7e6c200c4f77cfdc7f8dc84390475371cd8d5c0f
Sanjid Khan <sksanjid295@gmail.com>
2026-09-23 23:59:57 +0600

    Basic React setup

---
+++ 01_start_reactjs/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Learn React with Khan Chaicode.com</h1>
+      <p>This is a sub heading.</p>
+    </>
+  )
+}
+
+export default App
